9212c4ad6e9337b35f8979f705369a4b5142bee2
mxdi9i7 <peter.zheng88228@gmail.com>
2016-10-17 12:14:24 -0400

    asdf

--- inc/header.php
+++ inc/header.php
@@ -5,7 +5,7 @@
 	<!-- Compiled and minified CSS -->
   	<link rel="stylesheet" href="../css/materialize.css">
   	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  	
+
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   	<link rel="icon" type="img/png" href="../img/logo.png">
 </head>
@@ -14,22 +14,22 @@
   <nav class="white">
     <div class="nav-wrapper">
       <img src="../img/logo.png" class="brand-logo">
-      <a id="test" href="#" class="brand-logo grey-text darken-5">
+      <a id="test" href="#" class="brand-logo black-text">
       	Lanyu
       </a>
 
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons grey-text darken-5">menu</i></a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons black-text">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="index.php" class="grey-text darken-5">Home</a></li>
-        <li><a href="about.php" class="grey-text darken-5">About</a></li>
-        <li><a href="showroom.php" class="grey-text darken-5">Showroom</a></li>
-        <li><a href="contact.php" class="grey-text darken-5">Contact</a></li>
+        <li><a href="index.php" class="black-text">Home</a></li>
+        <li><a href="about.php" class="black-text">About</a></li>
+        <li><a href="showroom.php" class="black-text">Showroom</a></li>
+        <li><a href="contact.php" class="black-text">Contact</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
-        <li><a href="index.php" class="grey-text darken-5">Home</a></li>
-        <li><a href="about.php" class="grey-text darken-5">About</a></li>
-        <li><a href="showroom.php" class="grey-text darken-5">Showroom</a></li>
-        <li><a href="contact.php" class="grey-text darken-5">Contact</a></li>
+        <li><a href="index.php" class="black-text">Home</a></li>
+        <li><a href="about.php" class="black-text">About</a></li>
+        <li><a href="showroom.php" class="black-text">Showroom</a></li>
+        <li><a href="contact.php" class="black-text">Contact</a></li>
       </ul>
     </div>
   </nav>

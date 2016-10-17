4de46b04fcaf509866f3ac18ee676cebe806d506
mxdi9i7 <peter.zheng88228@gmail.com>
2016-10-17 12:05:31 -0400

    adf

--- inc/footer.php
+++ inc/footer.php
@@ -2,30 +2,40 @@
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="grey-text text-darken-4">Footer Content</h5>
-                <p class="grey-text text-darken-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 class="grey-text text-darken-4">Lanyu</h5>
+                <p class="grey-text text-darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
               <div class="col l4 offset-l2 s12">
                 <h5 class="grey-text text-darken-4">Site Map</h5>
                 <ul>
-                  <li><a class="grey-text text-darken-4" href="#!">Home</a></li>
-                  <li><a class="grey-text text-darken-4" href="#!">About</a></li>
-                  <li><a class="grey-text text-darken-4" href="#!">Showroom</a></li>
-                  <li><a class="grey-text text-darken-4" href="#!">Contact</a></li>
+                  <li><a class="grey-text text-darken-4" href="index.php">Home</a></li>
+                  <li><a class="grey-text text-darken-4" href="about.php">About</a></li>
+                  <li><a class="grey-text text-darken-4" href="showroom.php">Showroom</a></li>
+                  <li><a class="grey-text text-darken-4" href="contact.php">Contact</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div class="footer-copyright grey lighten-4">
             <div class="container grey-text text-darken-4">
-            © 2014 Copyright Text
-            <a class="grey-text text-darken-4 right" href="#!">More Links</a>
+            © 2016 Lanyu Couture Inc.
+            <ul class="right">
+            	<li><i class="fa fa-facebook grey-text text-darken-4 "></i></li>
+            	<li><i class="fa fa-instagram grey-text text-darken-4 "></i></li>
+            	<li><i class="fa fa-wechat grey-text text-darken-4 "></i></li>
+            	<li><i class="fa fa-twitter grey-text text-darken-4 "></i></li>
+            </ul>
             </div>
           </div>
         </footer>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
 <script type="text/javascript" src="../js/index.js"></script>
-
+<script src="https://use.fontawesome.com/570079994e.js"></script>
 </body>
 </html>
